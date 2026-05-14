b192972b734cf2809a017e73494265d8565e0531
Rey <30968847+reyvermouth@users.noreply.github.com>
2026-05-14 16:12:42 +0800

    2nd commit
    Added input field, start and quit button to main menu. Able to enter the game with start button and stop the game in editor with quit button

---
+++ Assets/Scripts/StartMenuUI.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity Player
+#endif
+    }
+}
